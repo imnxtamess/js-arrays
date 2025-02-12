a4b9e66cbcf629c8c901cc980904430d2fdb9945
imnxtamess <davide.morisco.work@gmail.com>
2025-02-12 13:38:31 +0100

    add 4th task

--- arrays/01-arrays/script.js
+++ arrays/01-arrays/script.js
@@ -24,9 +24,14 @@ console.log(teachers)
 console.log(lastTeacher)
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift()
+console.log(teachers);
+console.log(firstTeacher);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+
+
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
