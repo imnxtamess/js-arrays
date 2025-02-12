f525427a7032f3a186e317e5754014aa6b3e97dd
imnxtamess <davide.morisco.work@gmail.com>
2025-02-12 14:11:19 +0100

    add 2nd bonus

--- arrays/02-arrays-bonus/script.js
+++ arrays/02-arrays-bonus/script.js
@@ -16,9 +16,22 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  const teacher_name = teachers[i]
+  console.log(teacher_name);
+  if (teacher_name.length >= 5) {
+    longNames.push(teacher_name);
+  }
+}
+
+console.log(longNames);
+
 
 // 3. Rimuovi 'Ed' dall'array teachers
+
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
