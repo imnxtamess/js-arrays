742665443e4f71001ea6f9c107a6eb10159dff27
imnxtamess <davide.morisco.work@gmail.com>
2025-02-12 13:19:26 +0100

    add 2st task

--- arrays/01-arrays/script.js
+++ arrays/01-arrays/script.js
@@ -13,7 +13,9 @@ const teachers = [
 const fourthTeacher = teachers[3];
 console.log(fourthTeacher);
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-
+const fifthTeacher = teachers[4];
+teachers[4] = "Patrick"
+console.log(teachers);
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
 
